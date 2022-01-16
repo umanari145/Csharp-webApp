4ae76b6e00cccf60acabe8a4f90b85ba4c8f24ad
matsumoto norio <umanari145@gmail.com>
2022-01-16 10:58:26 +0900

    first commit

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class ItemController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public ItemController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("/Item/List")]
+        public IActionResult GetItemsList()
+        {
+            _logger.Info("info log.");
+            return View();
+        }
+    }
+}
